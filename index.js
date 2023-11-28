105dcb65d65014104ae57950d34b967fad224191
Johad Toledo <johadtledo@gmail.com>
2023-11-28 18:38:37 +1000

    add git ignore, mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const colors = require('colors');
+
+
+  const { MongoClient, ServerApiVersion } = require('mongodb');
+  const uri = "mongodb+srv://johadtledo:pI8JZHJv78tE5uZs@cluster0.vif9moa.mongodb.net/?retryWrites=true&w=majority";
+  
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+  
+
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
@@ -15,7 +45,11 @@ const server = http.createServer((req, res) => {
         res.end(data);
       }
     });
-  } else {
+} else if (req.url === '/about') {
+  // Handle the /about route
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('About Us Page');}
+  else {
     // Handle other routes
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
@@ -25,6 +59,7 @@ const server = http.createServer((req, res) => {
 const PORT = 3000;
 const IP = '127.0.0.1';
 
+
 server.listen(PORT, IP, () => {
-  console.log(`Server running at http://${IP}:${PORT}/`);
+  console.log(`Server running at`.green, `http://${IP}:${PORT}/`.cyan);
 });
